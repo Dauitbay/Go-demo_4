92f888c419d94ccf4a03df93eb430d629d73772c
“firstname <dawitsarsenbaev@gmail.com>
2024-11-01 21:11:20 +0500

    Error handling and validation

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math/rand/v2"
+	"net/url"
 )
 
 type account struct {
@@ -24,12 +26,25 @@ func (acc *account) generatePassword(n int) {
 	acc.password = string(res)
 }
 
-func newAccount(login, password, url string) *account {
-	return &account{
-		password: password,
-		url: url,
-		login: login,
+func newAccount(login, password, urlString string) (*account, error) {
+	if login == "" {
+		return nil, errors.New("INVALID LOGIN")
 	}
+	_, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return nil, errors.New("INVALID URL")
+	}
+	newAcc := &account{
+		login: login,
+		password: password,
+		url: urlString,
+	}
+
+	if password == "" {
+		var newP int = 12
+		newAcc.generatePassword(newP)
+	}
+	return newAcc, nil
 }
 
 var letterRunes = []rune("abcdfghijklmnopqrstuvwxyzABCDFGHIJKLMNOPQRSTUVWXYZ1234567890-*!")
@@ -40,16 +55,18 @@ func main() {
 	password := prompData("Enter your password")
 	url := prompData("Enter your url")
 
-	myAccount := newAccount(login, password, url)
-	var newP int = 12
-	myAccount.generatePassword(newP)
+	myAccount, err := newAccount(login, password, url)
+	if err != nil {
+		fmt.Println("Invalid url or login")
+		return
+	}
 	myAccount.outputPassword()
 }
 
 func prompData(promp string) string {
 	fmt.Print(promp + " ")
 	var res string
-	fmt.Scan(&res)
+	fmt.Scanln(&res)
 	return res
 }
 
