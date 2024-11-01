7d24ccfb2a1f5786ff69be1672c95ffae1da7465
“firstname <dawitsarsenbaev@gmail.com>
2024-11-01 21:36:57 +0500

    Seperate main.go package to account.go

--- main.go
+++ main.go
@@ -1,66 +1,21 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"math/rand/v2"
-	"net/url"
 )
 
-type account struct {
-	login string
-	password string
-	url string
-}
-
-func (acc *account) outputPassword() {
-	fmt.Println(acc)
-	fmt.Println(acc.login, acc.password, acc.url)
-}
-
-func (acc *account) generatePassword(n int) {
-	res := make([]rune , n)
-	for i := range res {
-		res[i] = letterRunes[rand.IntN(len(letterRunes))]
-	}
-	acc.password = string(res)
-}
-
-func newAccount(login, password, urlString string) (*account, error) {
-	if login == "" {
-		return nil, errors.New("INVALID LOGIN")
-	}
-	_, err := url.ParseRequestURI(urlString)
-	if err != nil {
-		return nil, errors.New("INVALID URL")
-	}
-	newAcc := &account{
-		login: login,
-		password: password,
-		url: urlString,
-	}
-
-	if password == "" {
-		var newP int = 12
-		newAcc.generatePassword(newP)
-	}
-	return newAcc, nil
-}
-
-var letterRunes = []rune("abcdfghijklmnopqrstuvwxyzABCDFGHIJKLMNOPQRSTUVWXYZ1234567890-*!")
-
 func main() {
-	
+
 	login := prompData("Enter your login: ")
 	password := prompData("Enter your password")
 	url := prompData("Enter your url")
 
-	myAccount, err := newAccount(login, password, url)
+	myAccount, err := newAccountWithTimeStamp(login, password, url)
 	if err != nil {
 		fmt.Println("Invalid url or login")
 		return
 	}
-	myAccount.outputPassword()
+	myAccount.acc.outputPassword()
 }
 
 func prompData(promp string) string {
@@ -69,4 +24,3 @@ func prompData(promp string) string {
 	fmt.Scanln(&res)
 	return res
 }
-
