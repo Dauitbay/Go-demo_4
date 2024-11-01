b096aab4d9e21768d56510743a115e13c04d2923
“firstname <dawitsarsenbaev@gmail.com>
2024-11-01 20:42:16 +0500

    Add methods to account

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand/v2"
+)
 
 type account struct {
 	login string
@@ -8,17 +11,35 @@ type account struct {
 	url string
 }
 
+func (acc *account) outputPassword() {
+	fmt.Println(acc)
+	fmt.Println(acc.login, acc.password, acc.url)
+}
+
+func (acc *account) generatePassword(n int) {
+	res := make([]rune , n)
+	for i := range res {
+		res[i] = letterRunes[rand.IntN(len(letterRunes))]
+	}
+	acc.password = string(res)
+}
+
+var letterRunes = []rune("abcdfghijklmnopqrstuvwxyzABCDFGHIJKLMNOPQRSTUVWXYZ1234567890-*!")
+
 func main() {
+	
 	login := prompData("Enter your login: ")
-	password := prompData("Enter your password")
+	// password := prompData("Enter your password")
 	url := prompData("Enter your url")
 
 	myAccount := account {
-		password: password,
+		// password: password,
 		url: url,
 		login: login,
 	}
-	outputPassword(&myAccount)
+	var newP int = 12
+	myAccount.generatePassword(newP)
+	myAccount.outputPassword()
 }
 
 func prompData(promp string) string {
@@ -28,7 +49,3 @@ func prompData(promp string) string {
 	return res
 }
 
-func outputPassword(acc *account) {
-	fmt.Println(acc)
-	fmt.Println(acc.login, acc.password, acc.url)
-}
