546eca429b6026cc1c082aaa5710785131554bab
“firstname <dawitsarsenbaev@gmail.com>
2024-11-02 23:36:30 +0500

    Updated file write and read

--- main.go
+++ main.go
@@ -7,13 +7,45 @@ import (
 )
 
 func main() {
+	fmt.Println("Manager of passwords")
+Menu:
+	for {
+		variant := getMenu()
+		switch variant {
+		case 1:
+			createAccount()
+		case 2:
+			findAccount()
+		case 3:
+			deleteAccount()
+		default:
+			break Menu
+		}
+	}
 	createAccount()
 }
 
+func getMenu() int {
+	var variant int
+	fmt.Println("Choose variant")
+	fmt.Println("1. Create an account")
+	fmt.Println("2. Search an account")
+	fmt.Println("3. Delete an account")
+	fmt.Println("4. Exit")
+	fmt.Scanln(&variant)
+	return variant
+}
+
+func findAccount() {
+
+}
+func deleteAccount() {
+
+}
 func createAccount() {
 	// files.WriteFile("Hello i am file", "file.txt")
 	// files.ReadFile()
-	login := prompData("Enter your login: ")
+	login := prompData("Enter your login")
 	password := prompData("Enter your password")
 	url := prompData("Enter your url")
 
@@ -22,16 +54,18 @@ func createAccount() {
 		fmt.Println("Invalid url or login")
 		return
 	}
-	file, err := myAccount.ToBytes()
+	vault := account.NewVault()
+	vault.AddAccount(*myAccount)
+	data, err := vault.ToBytes()
 	if err != nil {
 		fmt.Println("Could not convert to JSON")
 		return
 	}
-	files.WriteFile(file, "data.json")
+	files.WriteFile(data, "data.json")
 }
 
 func prompData(promp string) string {
-	fmt.Print(promp + " ")
+	fmt.Println(promp + ": ")
 	var res string
 	fmt.Scanln(&res)
 	return res
